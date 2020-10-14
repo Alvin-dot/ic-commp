9d7e2bfafa1f6a2dd73390ecacf6473b79190fe9
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-10-14 15:28:47 -0300

    Used real fast fourier transform implemented Used pandas to import CSV file Change plot tool to bokeh and added hover tool

--- main.py
+++ main.py
@@ -1,88 +1,150 @@
-import csv
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool
+from bokeh.models.tickers import AdaptiveTicker
+from bokeh.layouts import column
 
-# Abre o arquivo CSV como uma lista de dicionários
-with open('515_new.csv') as csv_file:
-    origin_file = list(csv.DictReader(csv_file))
+# Importa o arquivo CSV como um array
+origin_file = pd.read_csv('515_new.csv', na_filter=False)
 
 # Separa os valores de tempo e frequência em suas próprias listas
-date_values = [d['Date'] for d in origin_file]
-frequency_values = [f['Value'] for f in origin_file]
-
-# Transforma os valores de frequência de 'string' para 'float'
-frequency_values_float = [float(f) for f in frequency_values if f != ""]
+date_values = np.array([d for d in origin_file['Date']])
+frequency_values = np.array([f for f in origin_file['Value'] if f != ""])
 
 # Processamento dos dados
 
-# Número total de pontos no arquivo
-n = len(frequency_values_float)
+# Número total de pontos no arquivo original
+n = len(frequency_values)
 
 # Período de amostragem
-p = 1/120
+p = 1 / 120
 
-# Transformada de fourier do sinal
-signal_fft = np.fft.fft(frequency_values_float, n)
+# Transformada de fourier do sinal real
+signal_fft = np.fft.rfft(frequency_values, n)
 
 # Obtenção do módulo e ângulo do número complexo
 signal_fft_module = np.abs(signal_fft)
 signal_fft_angle = np.angle(signal_fft, deg=True)
 
 # Obtenção das frequências pertencentes ao eixo horizontal
-frequency_fft = np.linspace(0, 2*np.pi/p, num=n, endpoint=False)
+frequency_fft = np.linspace(0, 2 * np.pi / p, num=len(signal_fft), endpoint=False)
 
 # Filtro para valores muito altos do módulo da FFT
 filt = signal_fft_module < 5000
 
-# Cálculo para o espectro de frequência do sinal
+# Cálculo para o espectro de potência do sinal
 power_spec = signal_fft_module ** 2
 
-# Gráfico do sinal de frequência original
-fig_original, ax0 = plt.subplots(figsize=(14, 6))
-ax0.plot(range(len(frequency_values_float)), frequency_values_float)
-ax0.set_title('Sinal de frequência original')
-ax0.set_xlabel('Número de amostras')
-ax0.set_ylabel('Frequência [Hz]')
-ax0.grid(alpha=0.3)
+# Gráficos e Resultados
 
-plt.show()
+# Configura exportação dos resultados
+output_file('results.html')
 
-# Gráfico do módulo e ângulo da FFT sem filtro
-fig_no_filter, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 6))
-fig_no_filter.suptitle('FFT do sinal de frequência sem filtro')
-ax1.plot(frequency_fft, signal_fft_module)
-ax1.set_ylabel('Módulo')
-ax1.grid(alpha=0.3)
-ax2.plot(frequency_fft, signal_fft_angle)
-ax2.set_ylabel('Ângulo [°]')
-ax2.set_xlabel('Frequência [rad/s]')
-ax2.grid(alpha=0.3)
+# Gráfico sinal original
+original_signal_df = pd.DataFrame({'Frequência[Hz]': frequency_values,
+                                   'Número de amostras': range(len(frequency_values))})
 
-plt.show()
+tt_01 = HoverTool(tooltips=[
+    ("Frequência[Hz]", "@{Frequência[Hz]}"),
+    ("Número da amostra", "@{Número de amostras}")
+])
 
-# Gráfico do módulo e ângulo da FFT com filtro filtro
-fig_filter, (ax3, ax4) = plt.subplots(2, 1, figsize=(14, 6))
-fig_filter.suptitle('FFT do sinal de frequência filtrado')
-ax3.plot(frequency_fft[filt], signal_fft_module[filt])
-ax3.set_ylabel('Módulo')
-ax3.grid(alpha=0.3)
-ax4.plot(frequency_fft[filt], signal_fft_angle[filt])
-ax4.set_ylabel('Ângulo [°]')
-ax4.set_xlabel('Frequência [rad/s]')
-ax4.grid(alpha=0.3)
+original_signal_plot = figure(title="Sinal de Frequência Original",
+                              x_axis_label='Número de amostras',
+                              y_axis_label='Frequência[Hz]',
+                              plot_width=1200,
+                              plot_height=600,
+                              tools=[tt_01])
+original_signal_plot.line('Número de amostras', 'Frequência[Hz]', line_width=2, source=original_signal_df, alpha=0.8)
+original_signal_plot.yaxis.ticker = AdaptiveTicker(desired_num_ticks=10)
+original_signal_plot.title.text_font_size = '16pt'
+original_signal_plot.axis.axis_label_text_font_style = 'bold'
 
-plt.show()
+# Gráfico fft sem filtro
+no_filter_module_fft = pd.DataFrame({'Módulo': signal_fft_module,
+                                     'Frequência[rad/s]': frequency_fft})
 
-# Gráfico do espectro de potência do sinal
-fig_power, ax5 = plt.subplots(figsize=(14, 6))
-ax5.plot(frequency_fft[filt], power_spec[filt])
-ax5.set_title('Espectro de potência do sinal de frequência filtrado')
-ax5.set_xlabel('Frequência [rad/s]')
-ax5.set_ylabel('Espectro de potência')
-ax5.grid(alpha=0.3)
+tt_02 = HoverTool(tooltips=[
+    ("Módulo", "@{Módulo}"),
+    ("Frequência[rad/s]", "@{Frequência[rad/s]}")
+])
 
-plt.show()
+no_filter_module_plot = figure(title="Transformada de Fourier - Sem filtro",
+                               y_axis_label='Módulo',
+                               plot_width=1200,
+                               plot_height=400,
+                               tools=[tt_02])
+no_filter_module_plot.line('Frequência[rad/s]', 'Módulo', line_width=2, source=no_filter_module_fft, alpha=0.8)
+no_filter_module_plot.title.text_font_size = '16pt'
+no_filter_module_plot.axis.axis_label_text_font_style = 'bold'
 
+no_filter_angle_fft = pd.DataFrame({'Ângulo[°]': signal_fft_angle,
+                                    'Frequência[rad/s]': frequency_fft})
+
+tt_03 = HoverTool(tooltips=[
+    ("Ângulo[°]", "@{Ângulo[°]}"),
+    ("Frequência[rad/s]", "@{Frequência[rad/s]}")
+])
+
+no_filter_angle_plot = figure(title="",
+                              x_axis_label='Frequência[rad/s]',
+                              y_axis_label='Ângulo[°]',
+                              plot_width=1200,
+                              plot_height=400,
+                              tools=[tt_03])
+no_filter_angle_plot.line('Frequência[rad/s]', 'Ângulo[°]', line_width=2, source=no_filter_angle_fft, alpha=0.8,
+                          line_color='orange')
+no_filter_angle_plot.axis.axis_label_text_font_style = 'bold'
+
+# Gráfico fft filtrado
+filtered_module_fft = pd.DataFrame({'Módulo': signal_fft_module[filt],
+                                    'Frequência[rad/s]': frequency_fft[filt]})
+
+filtered_module_plot = figure(title="Transformada de Fourier - Filtrado",
+                              y_axis_label='Módulo',
+                              plot_width=1200,
+                              plot_height=420,
+                              tools=[tt_02])
+filtered_module_plot.line('Frequência[rad/s]', 'Módulo', line_width=2, source=filtered_module_fft, alpha=0.8)
+filtered_module_plot.title.text_font_size = '16pt'
+filtered_module_plot.axis.axis_label_text_font_style = 'bold'
+
+filtered_angle_fft = pd.DataFrame({'Ângulo[°]': signal_fft_angle[filt],
+                                   'Frequência[rad/s]': frequency_fft[filt]})
+
+filtered_angle_plot = figure(x_axis_label='Frequência[rad/s]',
+                             y_axis_label='Ângulo[°]',
+                             plot_width=1200,
+                             plot_height=400,
+                             tools=[tt_03])
+filtered_angle_plot.line('Frequência[rad/s]', 'Ângulo[°]', line_width=2, source=filtered_angle_fft, alpha=0.8,
+                         line_color='orange')
+filtered_angle_plot.axis.axis_label_text_font_style = 'bold'
+
+# Gráfico espectro de potência
+ps_signal_df = pd.DataFrame({'Espectro de Potência': power_spec[filt],
+                             'Frequência[rad/s]': frequency_fft[filt]})
+
+tt_04 = HoverTool(tooltips=[
+    ("Espectro de Potência", "@{Espectro de Potência}"),
+    ("Frequência[rad/s]", "@{Frequência[rad/s]}")
+])
+
+ps_signal_plot = figure(title="Espectro de Potência",
+                        x_axis_label='Frequência[rad/s]',
+                        y_axis_label='Espectro de Potência',
+                        plot_width=1200,
+                        plot_height=600,
+                        tools=[tt_04])
+ps_signal_plot.line('Frequência[rad/s]', 'Espectro de Potência', line_width=2, source=ps_signal_df, alpha=0.8)
+ps_signal_plot.title.text_font_size = '16pt'
+ps_signal_plot.axis.axis_label_text_font_style = 'bold'
+
+show(column(original_signal_plot,
+            no_filter_module_plot, no_filter_angle_plot,
+            filtered_module_plot, filtered_angle_plot,
+            ps_signal_plot))
 
 '''
 Frequência de amostragem de 120 Hz
