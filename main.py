ec73acf6e91adcc644068f45f11a96a49b5dd591
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-11-03 21:25:25 -0300

    begin implementation of Plotly module

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from get_data import get_data_from_api
 from signal_processing import get_frequency_fft
 from get_plots import plot_config, show_plots
 from datetime import datetime
+import plotly.express as px
+import numpy as np
 
 # Get start and end time
-start_time_str = datetime.strptime('02.11.2020 10:00:00', '%d.%m.%Y %H:%M:%S')
+start_time_str = datetime.strptime('03.11.2020 13:00:00', '%d.%m.%Y %H:%M:%S')
 start_time_unix = start_time_str.timestamp() * 1000
 
-end_time_str = datetime.strptime('02.11.2020 12:00:00', '%d.%m.%Y %H:%M:%S')
+end_time_str = datetime.strptime('03.11.2020 15:00:00', '%d.%m.%Y %H:%M:%S')
 end_time_unix = end_time_str.timestamp() * 1000
 
 data_freq = 120
@@ -15,9 +17,12 @@ data_freq = 120
 # Get the frequency data based on the start and end time
 api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1)
 
-# Converts unix time from GMT time to local time
-unix_time_values = [i[0] - (3 * 3600000) for i in api_data]
+# Converts unix time from GMT time to local time and removes milisseconds
+unix_time_values = [(i[0] - (3 * 3600000)) / 1000 for i in api_data]
 frequency_values = [i[1] for i in api_data]
+
+# Converts unix time to Numpy DateTime64 time
+time_values = [np.datetime64(int(i), 's') for i in unix_time_values]
 
 # Process the fft of the signal
 fft_value = get_frequency_fft(frequency_values, sample_frequency=data_freq)
@@ -25,6 +30,13 @@ fft_value = get_frequency_fft(frequency_values, sample_frequency=data_freq)
 # Calculates the power spec of the fft
 power_spec = fft_value['Módulo'] ** 2
 
+fig = px.scatter(x=time_values, y=frequency_values, labels={'x': 'Tempo', 'y': 'Frequência [Hz]'})
+fig.show()
+
+fig = px.scatter(x=[1, 2, 3], y=[1, 2, 3], labels={'x': 'Hey', 'y': 'Hi [Hz]'})
+fig.show()
+
+'''
 # Original frequency signal plot
 frequency_plot = plot_config(plot_title=f'Frequência da Rede @{data_freq} Hz',
                              x_axis_label='Tempo',
@@ -53,3 +65,4 @@ power_spec_plot = plot_config(plot_title=f'Espectro de Potência @{data_freq} Hz
 
 # Show all plot on a column
 show_plots(frequency_plot, fft_plot, power_spec_plot)
+'''
