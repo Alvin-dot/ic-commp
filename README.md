fd98349737434f1ec0fa27930a0cdc4d18ce35af
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-11-16 16:53:38 -0300

    adds milisseconds to frequency graph, adds new method of replacing None values, defined time window and FFT method for online application

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ic-commp
 
 TODO LIST
-- [X] Aplicar filtros presentes nos papers
-- [ ] Estudar sobre filtros FIR e transformada de Welch
-- [ ] processamento e resultados
+- [X] Plotar milissegundos no gráfico de frequência
+- [ ] Definir janela de dados e tempo de atualização
+- [X] Configurar API para vir os valores como NaN
+- [ ] Fazer um While True com intervalo de tempo para atualização dos dados
+- [ ] Salvar figura do Plotly em png
+- [ ] Escrever texto com o que o programa faz e nome completo
 - [ ] Colocar algoritmo no servidor online
-- [ ] Plotar milissegundos no gráfico de frequência
+
--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import pandas as pd
 from scipy import signal, stats
 
 # Get start and end time in unix time milisseconds
-start_time_str = datetime.strptime('12.11.2020 14:00:00', '%d.%m.%Y %H:%M:%S')
+start_time_str = datetime.strptime('14.11.2020 05:00:00', '%d.%m.%Y %H:%M:%S')
 start_time_unix = start_time_str.timestamp() * 1000
 
-end_time_str = datetime.strptime('12.11.2020 16:00:00', '%d.%m.%Y %H:%M:%S')
+end_time_str = datetime.strptime('14.11.2020 06:00:00', '%d.%m.%Y %H:%M:%S')
 end_time_unix = end_time_str.timestamp() * 1000
 
 # Frequency value derived from 4.1.5 downsampling
 data_freq = 5
 
 # Get the frequency data based on the start and end time
-api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1)
+api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1, skip_missing=0)
 
 unix_time_values = [i[0] for i in api_data]
 frequency_values = [i[1] for i in api_data]
 
 # Converts unix time from GMT time to local time and removes milisseconds
-time_values = [(i - (3 * 3600000)) / 1000 for i in unix_time_values]
+time_values = [(i - (3 * 3600000)) for i in unix_time_values]
 # Converts unix time to Numpy DateTime64 time
-time_values = [np.datetime64(int(i), 's') for i in time_values]
+time_values = [np.datetime64(int(i), 'ms') for i in time_values]
 
 # Creates dataframe for variables
 df = pd.DataFrame({"freq": frequency_values, "date": time_values, "original_freq": frequency_values})
@@ -35,11 +35,7 @@ df = pd.DataFrame({"freq": frequency_values, "date": time_values, "original_freq
 # -----------------------------------
 
 # Replaces None values with NaN
-none_counter = 0
-for i in df["freq"]:
-    if i is None:
-        df["freq"][none_counter] = np.NaN
-    none_counter += 1
+df["freq"].fillna(np.NaN)
 
 # -----------------------------------
 # Treatment of outliers
@@ -78,36 +74,34 @@ df.loc[:, "freq"] -= df["freq"].mean()
 # Filtering
 # -----------------------------------
 
+# Application of a FIR bandpass filter
 h = np.float32(signal.firwin(numtaps=2500, cutoff=(0.1, 2), window='hann', pass_zero='bandpass',
                              scale=False, fs=data_freq))
 
 df["freq_filter"] = signal.filtfilt(h, 1, df["freq"])
 
+# Take the signal derivative for detrending purposes
+db = np.diff(df["freq_filter"], n=1)
+
 # -----------------------------------
 # FFT calculation
 # -----------------------------------
 
-db = np.diff(df["freq_filter"], n=1)
-
 # fft_module, fft_freq, fft_angle = get_frequency_fft(db, data_freq)
 
-
-fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq_filter"]))//50,
+fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq_filter"]))//20,
                                     average='mean', detrend='constant')
 
 # -----------------------------------
 # Plotting
 # -----------------------------------
-'''
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=time_values, y=df["freq"], mode='lines', name='sinal original'))
 fig.add_trace(go.Scatter(x=time_values, y=df["freq_filter"], mode='lines', name='sinal filtrado'))
 fig.update_layout(title="Gráfico da frequência da rede no tempo", xaxis_title="Tempo", yaxis_title="Frequência [Hz]")
 fig.show()
-'''
+
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=fft_freq, y=fft_module, mode='markers'))
 fig.update_layout(title="Transformada de Welch", xaxis_title="Frequência [Hz]", yaxis_title="Módulo")
-fig.add_annotation(text='Dados obtidos utilizando média movel, filtro FIR e diferenciação',
-                   xref='paper', yref='paper', x=0.001, y=1.05, showarrow=False)
 fig.show()
