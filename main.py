9907630983f1507cd75b3f22c8f3c0b3b17a8768
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-11-23 10:20:44 -0300

    writes values in a csv file for front end plotting

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from get_data import get_data_from_api
 from datetime import datetime
 from scipy import signal
-import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
 import time
-
 
 # Sampling rate given in Hz
 data_freq = 5
@@ -35,7 +33,8 @@ while True:
         end_time_unix += refresh_time_window * 60 * 1000
 
         # Get the frequency data based on the start and end time
-        api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1, skip_missing=0)
+        api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1,
+                                     skip_missing=0)
 
         # Replaces data into lists
         del unix_time_values[:(refresh_time_window * 60 * data_freq) - 1]
@@ -54,44 +53,44 @@ while True:
     # -----------------------------------
     # Treatment of missing data
     # -----------------------------------
-    
+
     # Replaces None values with NaN
     df["freq"].fillna(np.NaN, inplace=True)
-    
+
     # -----------------------------------
     # Treatment of outliers
     # -----------------------------------
-    
+
     # ROLLING MEAN AND STANDARD DEVIATION METHOD
     roll_window = 20
     alpha = 3
-    
+
     # Calculate moving average
     df["roll_mean"] = df.freq.rolling(window=roll_window).mean()
-    
+
     # Pad first 20 values with same as the original data
     df.loc[:roll_window - 1, "roll_mean"] = df.loc[:roll_window - 1, "freq"]
-    
+
     for i in range(len(df["roll_mean"])):
         if not ((abs(df["roll_mean"][i]) + alpha * df["freq"].std()) > abs(df["freq"][i]) >
                 (abs(df["roll_mean"][i]) - alpha * df["freq"].std())):
             df.loc[i, "freq"] = np.NaN
-    
+
     # -----------------------------------
     # Interpolation process
     # -----------------------------------
-    
+
     # Replaces NaN values with mean value
     for i in range(len(df["freq"])):
         if df["freq"][i] != df["freq"][i]:
             df.loc[i, "freq"] = df["freq"].mean()
-    
+
     # -----------------------------------
     # Removal of signal average
     # -----------------------------------
 
     df.loc[:, "freq"] -= df["freq"].mean()
-    
+
     # -----------------------------------
     # Filtering
     # -----------------------------------
@@ -99,7 +98,7 @@ while True:
     # Application of a FIR bandpass filter
     h = np.float32(signal.firwin(numtaps=2500, cutoff=(0.1, 2), window='hann', pass_zero='bandpass',
                                  scale=False, fs=data_freq))
-    
+
     df["freq_filter"] = signal.filtfilt(h, 1, df["freq"])
 
     # Take the signal derivative for detrending purposes
@@ -110,23 +109,25 @@ while True:
     # -----------------------------------
 
     # Uses Welch method for FFT calculation
-    fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq"]))//20,
+    fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq"])) // 20,
                                         average='mean', detrend='constant')
-    
+
     # -----------------------------------
     # Plotting
     # -----------------------------------
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=time_values, y=df["original_freq"], mode='lines'))
-    fig.update_layout(title="Gráfico da frequência da rede no tempo", xaxis_title="Tempo", yaxis_title="Frequência [Hz]")
-    fig.write_image("/var/www/html/welch/Grafico1")
-    
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=fft_freq, y=fft_module, mode='lines+markers'))
-    fig.update_layout(title="Transformada de Welch", xaxis_title="Frequência [Hz]", yaxis_title="Módulo")
-    fig.write_image("/var/www/html/welch/Grafico2")
+
+    # Writes plot values in a csv file
+    with open('./var/www/html/welch/Grafico1.csv', 'w') as file:
+        for i in range(len(df['original_freq'])):
+            file.write('%s,%s\n' % (df['date'][i], df['original_freq'][i]))
+
+    with open('./var/www/html/welch/Grafico2.csv', 'w') as file:
+        for i in range(len(fft_module)):
+            file.write('%s,%s\n' % (fft_freq[i], fft_module[i]))
 
     reset_flag = False
+
+    print("awaiting...")
 
     # Awaits for determined refresh time
     time.sleep(refresh_time_window * 60)
