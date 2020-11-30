934d27a9a27cd683dcb0d1d6de0544bfd251599b
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2020-11-30 12:14:04 -0300

    Implements pmu change option

--- main.py
+++ main.py
@@ -5,136 +5,185 @@ import numpy as np
 import pandas as pd
 import time
 
-
-# Sampling rate given in Hz
-data_freq = 5
-# Set the data time window in hours
-data_time_window = 1
-# Set the refresh time window in minutes
-refresh_time_window = 5
-
-reset_flag = True
-
-# Get the hour time window from current time in unix milisseconds format
-end_time_str = datetime.now()
-end_time_unix = int(end_time_str.timestamp() * 1000)
-start_time_unix = end_time_unix - (data_time_window * 3600 * 1000)
-
-# Get the frequency data based on the start and end time
-api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1, skip_missing=0)
-
-unix_time_values = [i[0] for i in api_data]
-frequency_values = [i[1] for i in api_data]
-
+# Start program loop
 while True:
 
-    # Update status of the program
-    with open('/var/www/html/welch/status.csv', 'w+') as file:
-        file.write('loading')
+    # Sampling rate given in Hz
+    data_freq = 5
+    # Set the data time window in hours
+    data_time_window = 1
+    # Set the refresh time window in minutes
+    refresh_time_window = 5
 
-    if not reset_flag:
-        # Gets 5 minutes window for data refreshing
-        start_time_unix = end_time_unix
-        end_time_unix += refresh_time_window * 60 * 1000
+    reset_flag = True
 
-        # Get the frequency data based on the start and end time
-        api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1,
-                                     skip_missing=0)
+    # Update selected PMU variable
+    with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\pmu_port.csv', 'r') as file:
+        pmu_select = file.readline()
+        file.close()
 
-        # Replaces data into lists
-        del unix_time_values[:(refresh_time_window * 60 * data_freq) - 1]
-        del frequency_values[:(refresh_time_window * 60 * data_freq) - 1]
+    if pmu_select == 'eficiencia':
+        pmu_select = 506
+    elif pmu_select == 'cabine':
+        pmu_select = 515
+    elif pmu_select == 'patolina':
+        pmu_select = 524
+    elif pmu_select == 'agrarias':
+        pmu_select = 533
 
-        for i in api_data:
-            unix_time_values.append(i[0])
-            frequency_values.append(i[1])
+    # Get the hour time window from current time in unix milisseconds format
+    end_time_str = datetime.now()
+    end_time_unix = int(end_time_str.timestamp() * 1000)
+    start_time_unix = end_time_unix - (data_time_window * 3600 * 1000)
 
-    # Converts unix time to Numpy DateTime64 time milisseconds and converts from GMT time to local time
-    time_values = [np.datetime64(int(i - (3 * 3600000)), 'ms') for i in unix_time_values]
+    # Get the frequency data based on the start and end time
+    api_data = get_data_from_api(start_time_unix, end_time_unix, feed_id=pmu_select, interval=data_freq,
+                                 interval_type=1, skip_missing=0)
 
-    # Creates dataframe for variables
-    df = pd.DataFrame({"freq": frequency_values, "date": time_values, "original_freq": frequency_values})
+    unix_time_values = [i[0] for i in api_data]
+    frequency_values = [i[1] for i in api_data]
 
-    # -----------------------------------
-    # Treatment of missing data
-    # -----------------------------------
+    while True:
 
-    # Replaces None values with NaN
-    df["freq"].fillna(np.NaN, inplace=True)
+        # Update status of the program
+        with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\status.csv', 'w+') as file:
+            file.write('loading')
 
-    # -----------------------------------
-    # Treatment of outliers
-    # -----------------------------------
+        if not reset_flag:
+            # Gets 5 minutes window for data refreshing
+            start_time_unix = end_time_unix
+            end_time_unix += refresh_time_window * 60 * 1000
 
-    # ROLLING MEAN AND STANDARD DEVIATION METHOD
-    roll_window = 20
-    alpha = 3
+            # Get the frequency data based on the start and end time
+            api_data = get_data_from_api(start_time_unix, end_time_unix, interval=data_freq, interval_type=1,
+                                         skip_missing=0)
 
-    # Calculate moving average
-    df["roll_mean"] = df.freq.rolling(window=roll_window).mean()
+            # Replaces data into lists
+            del unix_time_values[:(refresh_time_window * 60 * data_freq) - 1]
+            del frequency_values[:(refresh_time_window * 60 * data_freq) - 1]
 
-    # Pad first 20 values with same as the original data
-    df.loc[:roll_window - 1, "roll_mean"] = df.loc[:roll_window - 1, "freq"]
+            for i in api_data:
+                unix_time_values.append(i[0])
+                frequency_values.append(i[1])
 
-    for i in range(len(df["roll_mean"])):
-        if not ((abs(df["roll_mean"][i]) + alpha * df["freq"].std()) > abs(df["freq"][i]) >
-                (abs(df["roll_mean"][i]) - alpha * df["freq"].std())):
-            df.loc[i, "freq"] = np.NaN
+        # Converts unix time to Numpy DateTime64 time milisseconds and converts from GMT time to local time
+        time_values = [np.datetime64(int(i - (3 * 3600000)), 'ms') for i in unix_time_values]
 
-    # -----------------------------------
-    # Interpolation process
-    # -----------------------------------
+        # Creates dataframe for variables
+        df = pd.DataFrame({"freq": frequency_values, "date": time_values, "original_freq": frequency_values})
 
-    # Replaces NaN values with mean value
-    for i in range(len(df["freq"])):
-        if df["freq"][i] != df["freq"][i]:
-            df.loc[i, "freq"] = df["freq"].mean()
+        # -----------------------------------
+        # Treatment of missing data
+        # -----------------------------------
 
-    # -----------------------------------
-    # Removal of signal average
-    # -----------------------------------
+        # Replaces None values with NaN
+        df["freq"].fillna(np.NaN, inplace=True)
 
-    df.loc[:, "freq"] -= df["freq"].mean()
+        # -----------------------------------
+        # Treatment of outliers
+        # -----------------------------------
 
-    # -----------------------------------
-    # Filtering
-    # -----------------------------------
+        # ROLLING MEAN AND STANDARD DEVIATION METHOD
+        roll_window = 20
+        alpha = 3
 
-    # Application of a FIR bandpass filter
-    h = np.float32(signal.firwin(numtaps=2500, cutoff=(0.1, 2), window='hann', pass_zero='bandpass',
-                                 scale=False, fs=data_freq))
+        # Calculate moving average
+        df["roll_mean"] = df.freq.rolling(window=roll_window).mean()
 
-    df["freq_filter"] = signal.filtfilt(h, 1, df["freq"])
+        # Pad first 20 values with same as the original data
+        df.loc[:roll_window - 1, "roll_mean"] = df.loc[:roll_window - 1, "freq"]
 
-    # Take the signal derivative for detrending purposes
-    db = np.diff(df["freq_filter"], n=1)
+        for i in range(len(df["roll_mean"])):
+            if not ((abs(df["roll_mean"][i]) + alpha * df["freq"].std()) > abs(df["freq"][i]) >
+                    (abs(df["roll_mean"][i]) - alpha * df["freq"].std())):
+                df.loc[i, "freq"] = np.NaN
 
-    # -----------------------------------
-    # FFT calculation
-    # -----------------------------------
+        # -----------------------------------
+        # Interpolation process
+        # -----------------------------------
 
-    # Uses Welch method for FFT calculation
-    fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq"])) // 20,
-                                        average='mean', detrend='constant')
+        # Replaces NaN values with mean value
+        for i in range(len(df["freq"])):
+            if df["freq"][i] != df["freq"][i]:
+                df.loc[i, "freq"] = df["freq"].mean()
 
-    # -----------------------------------
-    # Plotting
-    # -----------------------------------
+        # -----------------------------------
+        # Removal of signal average
+        # -----------------------------------
 
-    # Writes plot values in a csv file
-    with open('/var/www/html/welch/Grafico1.csv', 'w+') as file:
-        for i in range(len(df['original_freq'])):
-            file.write('%s,%s\n' % (df['date'][i], df['original_freq'][i]))
+        df.loc[:, "freq"] -= df["freq"].mean()
 
-    with open('/var/www/html/welch/Grafico2.csv', 'w+') as file:
-        for i in range(len(fft_module)):
-            file.write('%s,%s\n' % (fft_freq[i], fft_module[i]))
+        # -----------------------------------
+        # Filtering
+        # -----------------------------------
 
-    # Update status of the program
-    with open('/var/www/html/welch/status.csv', 'w+') as file:
-        file.write('working')
+        # Application of a FIR bandpass filter
+        h = np.float32(signal.firwin(numtaps=2500, cutoff=(0.1, 2), window='hann', pass_zero='bandpass',
+                                     scale=False, fs=data_freq))
 
-    reset_flag = False
+        df["freq_filter"] = signal.filtfilt(h, 1, df["freq"])
 
-    # Awaits for determined refresh time
-    time.sleep(refresh_time_window * 60)
+        # Take the signal derivative for detrending purposes
+        db = np.diff(df["freq_filter"], n=1)
+
+        # -----------------------------------
+        # FFT calculation
+        # -----------------------------------
+
+        # Uses Welch method for FFT calculation
+        fft_freq, fft_module = signal.welch(db, fs=data_freq, nperseg=(len(df["freq"])) // 20,
+                                            average='mean', detrend='constant')
+
+        # -----------------------------------
+        # Plotting
+        # -----------------------------------
+
+        # Writes plot values in a csv file
+        with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\Grafico1.csv', 'w+') as file:
+            for i in range(len(df['original_freq'])//10):
+                file.write('%s,%s\n' % (df['date'][i], df['original_freq'][i]))
+
+        with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\Grafico2.csv', 'w+') as file:
+            for i in range(len(fft_module)):
+                file.write('%s,%s\n' % (fft_freq[i], fft_module[i]))
+
+        # Update status of the program
+        with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\status.csv', 'w+') as file:
+            file.write('working')
+
+        reset_flag = False
+
+        # -----------------------------------
+        # Update from user check
+        # -----------------------------------
+
+        last_pmu = 'undefined'
+        refresh_flag = False
+        first_refresh_flag = False
+
+        # Awaits for determined user refresh time window
+        for count in range(refresh_time_window * 60 * 2):
+
+            # Checks current pmu selected
+            with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\pmu_port.csv', 'r') as file:
+                current_pmu = file.readline()
+                file.close()
+
+            # Checks if pmu has changed
+            if (current_pmu != last_pmu) and first_refresh_flag:
+                # Update status of the program
+                with open('C:\\Users\\alvar\\Desktop\\IC COMMP\\ic-commp-server\\status.csv', 'w+') as file:
+                    file.write('loading')
+                refresh_flag = True
+                break
+
+            last_pmu = current_pmu
+
+            first_refresh_flag = True
+
+            # Awaits 500 ms
+            time.sleep(0.5)
+
+        # Checks if program needs to be reset
+        if refresh_flag:
+            break
